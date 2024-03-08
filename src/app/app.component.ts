cd9e0cc24eb3e78884d998a719cf689a39985834
vlado-1 <vladosmolovic18@yahoo.com>
2024-03-08 20:37:26 +1100

    Created list and showcase components. Added cssl to split page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent, ShowcaseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
