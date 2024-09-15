57378f978f99936d49cb47861692251e60e4105e
vlado-1 <vladosmolovic18@yahoo.com>
2024-09-15 17:20:11 +1000

    Issue #8: Fixing logout to avoid logging out of google, and instead only from website.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { ShowcaseComponent } from './showcase/showcase.component';
@@ -12,4 +12,12 @@ import { ShowcaseComponent } from './showcase/showcase.component';
 })
 export class AppComponent {
   title = 'myweb';
+
+  onLogout(): void {
+    var googleButton: HTMLElement = <HTMLElement>document.getElementsByClassName("googleButton")[0];
+    var logoutButton: HTMLElement = <HTMLElement>document.getElementsByClassName("logout-btn")[0];   
+
+    googleButton.style.display = "flex";
+    logoutButton.style.display = "none";
+  }
 }
