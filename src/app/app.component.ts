9edd3c4001363481a11c82c397963cacc3ac5e40
vlado-1 <vladosmolovic18@yahoo.com>
2024-10-27 14:20:23 +1100

    Issue #7: Fixing Google Sign In button not appearing first time website accessed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,11 +20,22 @@ export class AppComponent implements OnInit{
 
   /** When app is initialized, check if the user is signed in on server */
   ngOnInit(): void {
+    this.loadGISLibrary();
     this.auth.isSignedIn();
   }
 
   /** Logout user from server */
   onLogout(): void {
     this.auth.logout();
+  }
+
+  /** Load the GIS lilbrary to enable Google Sign In Button */
+  loadGISLibrary () {
+    let node = document.createElement('script'); // creates the script tag
+    node.src = 'https://accounts.google.com/gsi/client'; // sets the source (insert url in between quotes)
+    node.type = 'text/javascript'; // set the script type
+    node.async = true; // makes script run asynchronously
+    // append to head of document
+    document.getElementsByTagName('head')[0].appendChild(node); 
   }
 }
